0c0deb7eb0547da012b3b4f8a8e4f79cf1502298
Matthew Nelson <developer@matthewnelson.io>
2024-02-01 07:46:57 -0800

    Add cInterop for apple spawn.h

--- library/process/build.gradle.kts
+++ library/process/build.gradle.kts
@@ -13,10 +13,59 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  **/
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+import org.jetbrains.kotlin.konan.target.HostManager
+import org.jetbrains.kotlin.konan.target.KonanTarget
+import org.jetbrains.kotlin.konan.target.Xcode
+
 plugins {
     id("configuration")
 }
 
 kmpConfiguration {
-    configureShared(publish = true) {}
+    configureShared(publish = true) {
+        kotlin {
+            targets.filterIsInstance<KotlinNativeTarget>().spawnCInterop()
+        }
+    }
+}
+
+fun List<KotlinNativeTarget>.spawnCInterop() {
+    if (!HostManager.hostIsMac) return
+    val xcode = Xcode.findCurrent()
+
+    forEach { target ->
+        val sdkInclude = when (target.konanTarget) {
+            is KonanTarget.IOS_ARM64 -> xcode.iphoneosSdk
+            is KonanTarget.IOS_SIMULATOR_ARM64,
+            is KonanTarget.IOS_X64 -> xcode.iphonesimulatorSdk
+
+            is KonanTarget.MACOS_ARM64,
+            is KonanTarget.MACOS_X64 -> xcode.macosxSdk
+
+            is KonanTarget.TVOS_ARM64 -> xcode.appletvosSdk
+            is KonanTarget.TVOS_SIMULATOR_ARM64,
+            is KonanTarget.TVOS_X64 -> xcode.appletvsimulatorSdk
+
+            is KonanTarget.WATCHOS_ARM32,
+            is KonanTarget.WATCHOS_ARM64,
+            is KonanTarget.WATCHOS_DEVICE_ARM64 -> xcode.watchosSdk
+            is KonanTarget.WATCHOS_SIMULATOR_ARM64,
+            is KonanTarget.WATCHOS_X64 -> xcode.watchsimulatorSdk
+            else -> return@forEach
+        }.let { sdkPath ->
+            File(sdkPath)
+                .resolve("usr")
+                .resolve("include")
+        }
+
+        target.compilations["main"].cinterops.create("spawn").apply {
+            includeDirs(sdkInclude.path)
+            defFile = projectDir
+                .resolve("src")
+                .resolve("nativeInterop")
+                .resolve("cinterop")
+                .resolve("spawn.def")
+        }
+    }
 }
