6d9d007a04cd01941237122f2e661dbe3284fc65
Matthew Nelson <developer@matthewnelson.io>
2024-02-26 15:31:45 -0500

    Add child/parent error communication for forkExec

--- library/process/src/unixTest/kotlin/io/matthewnelson/kmp/process/internal/ForkUnitTest.kt
+++ library/process/src/unixTest/kotlin/io/matthewnelson/kmp/process/internal/ForkUnitTest.kt
@@ -19,9 +19,12 @@ import io.matthewnelson.kmp.file.IOException
 import io.matthewnelson.kmp.process.IsDarwinMobile
 import io.matthewnelson.kmp.process.Signal
 import io.matthewnelson.kmp.process.Stdio
+import io.matthewnelson.kmp.process.internal.stdio.StdioHandle
 import io.matthewnelson.kmp.process.internal.stdio.StdioHandle.Companion.openHandle
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+import kotlin.test.fail
 
 class ForkUnitTest {
 
@@ -32,7 +35,7 @@ class ForkUnitTest {
             return
         }
 
-        val p = forkProcess("sh", listOf("-c", "sleep 1; exit 42;"))
+        val p = forkProcess("sh", listOf("-c", "sleep 1; exit 42"))
         val code = try {
             p.waitFor()
         } finally {
@@ -43,19 +46,59 @@ class ForkUnitTest {
         assertEquals(42, code)
     }
 
+    @Test
+    fun givenInvalidProgramPath_whenFork_thenExecThrowsException() {
+        if (IsDarwinMobile) {
+            println("Skipping...")
+            return
+        }
+
+        try {
+            // Should result in an ENOENT when exec is called
+            val p = forkProcess("/invalid/path/sh", listOf("-c", "sleep 1; exit 5"))
+            p.destroy()
+            fail("forkExec returned Process")
+        } catch (e: IOException) {
+            assertTrue(e.message!!.startsWith("Child process exec failure."))
+        }
+    }
+
+    @Test
+    fun givenBadDup2_whenFork_thenDup2ThrowsException() {
+        if (IsDarwinMobile) {
+            println("Skipping...")
+            return
+        }
+
+        try {
+            val h = Stdio.Config.Builder.get().build(null).openHandle()
+
+            // This should result in an IOException when dup2 is called in child process
+            // which will then be piped back to the parent process and child _exit called
+            h.close()
+
+            val p = forkProcess("sh", listOf("-c", "sleep 1; exit 5"), h)
+            p.destroy()
+            fail("forkExec returned Process")
+        } catch (e: IOException) {
+            assertTrue(e.message!!.startsWith("Child process dup2 failure."))
+        }
+    }
+
     private fun forkProcess(
         command: String,
         args: List<String>,
+        handle: StdioHandle? = null,
     ): NativeProcess {
-        val handle = Stdio.Config.Builder.get()
+        val h = handle ?: Stdio.Config.Builder.get()
             .build(null)
             .openHandle()
 
         val p = try {
             val b = PlatformBuilder.get()
-            b.forkExec(command, args, b.env, handle, Signal.SIGTERM)
+            b.forkExec(command, args, b.env, h, Signal.SIGTERM)
         } catch (e: IOException) {
-            handle.close()
+            h.close()
             throw e
         }
 
