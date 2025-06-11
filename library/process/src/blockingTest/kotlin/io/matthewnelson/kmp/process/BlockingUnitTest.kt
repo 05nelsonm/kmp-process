45190bfbd6b649c2091c5f0b1893a392823a3c3d
Matthew Nelson <developer@matthewnelson.io>
2025-06-11 13:31:20 -0400

    Add warm up cycles for unit tests

--- library/process/src/blockingTest/kotlin/io/matthewnelson/kmp/process/BlockingUnitTest.kt
+++ library/process/src/blockingTest/kotlin/io/matthewnelson/kmp/process/BlockingUnitTest.kt
@@ -26,24 +26,36 @@ class BlockingUnitTest {
     @Test
     fun givenThreadSleep_whenGreaterThan1second_thenSleepsApproximatelyThatDuration() {
         val duration = 2_400.milliseconds
+
+        // warm up
+        repeat(10) {
+            measureTime { Blocking.threadSleep(1.milliseconds) }
+        }
+
         val time = measureTime { Blocking.threadSleep(duration) }
 
         // Cannot be exact b/c we're talking about threads in a test here,
         // but can be close just to catch any critically wrong function logic.
-        val min = duration - 10.milliseconds
-        val max = duration + 10.milliseconds
+        val min = duration - 5.milliseconds
+        val max = duration + 150.milliseconds
         assertTrue(time in min..max, "${time.inWholeMilliseconds}ms")
     }
 
     @Test
     fun givenThreadSleep_whenLessThan1Second_thenSleepsApproximatelyThatDuration() {
         val duration = 50.milliseconds
+
+        // warm up
+        repeat(10) {
+            measureTime { Blocking.threadSleep(1.milliseconds) }
+        }
+
         val time = measureTime { Blocking.threadSleep(duration) }
 
         // Cannot be exact b/c we're talking about threads in a test here,
         // but can be close just to catch any critically wrong function logic.
-        val min = duration - 10.milliseconds
-        val max = duration + 10.milliseconds
+        val min = duration - 5.milliseconds
+        val max = duration + 15.milliseconds
         assertTrue(time in min..max, "${time.inWholeMilliseconds}ms")
     }
 
